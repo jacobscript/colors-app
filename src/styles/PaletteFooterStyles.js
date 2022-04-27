d4070be0bf202c8134b3460cbfcc7b3780193f6e
Jacob Lantsman <jacob.lantsman@gmail.com>
2022-04-26 20:48:55 -0400

    commit message

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,18 @@
+export default {
+    PaletteFooter: {
+        backgroundColor: 'white',
+        height: '5vh',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        // '& fis': {
+        //     fontSize: '1.5rem',
+        //     margin: '0 1rem',
+        // },
+    },
+    emoji: {
+        fontSize: '1.5rem',
+        margin: '0 1rem',
+    },
+}
